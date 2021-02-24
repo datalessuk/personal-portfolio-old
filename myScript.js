d37da2f91da10f2cf43c896ff74c0ca528f588f3
datalessuk <dataless@gmail.com>
2021-02-24 14:58:39 +0000

    added sweet alarts

--- myScript.js
+++ myScript.js
@@ -1,2 +1,14 @@
-//let newWindow = window.open;
+
+const emailPopUp = document.getElementById('myEmail');
+
+emailPopUp.addEventListener('click',myEmailPopUp);
+
+function myEmailPopUp(){
+    
+    Swal.fire({
+    icon: 'info',
+    title:'My Email',
+    text:'dataless@gmail.com'
+    })
+}
 
